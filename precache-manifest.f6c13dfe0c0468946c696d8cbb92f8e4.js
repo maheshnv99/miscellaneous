1526691a3a1487186376d3206229914c84199b3e
maheshnv99 <mahesh.nandini66@gmail.com>
2020-07-29 21:17:07 +0530

    Updates

---
+++ precache-manifest.f6c13dfe0c0468946c696d8cbb92f8e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a294a3beb47cff6592757ecb30ff1779",
+    "url": "/miscellaneous/index.html"
+  },
+  {
+    "revision": "ddbf5693134bf1bb80db",
+    "url": "/miscellaneous/static/css/main.9ff8a9da.chunk.css"
+  },
+  {
+    "revision": "52d26443c8b290270834",
+    "url": "/miscellaneous/static/js/2.1083dd99.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/miscellaneous/static/js/2.1083dd99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddbf5693134bf1bb80db",
+    "url": "/miscellaneous/static/js/main.7188de72.chunk.js"
+  },
+  {
+    "revision": "739d49f4c7a7069d5ce4",
+    "url": "/miscellaneous/static/js/runtime-main.85839549.js"
+  }
+]);
